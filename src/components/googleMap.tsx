1275d86d84cfb1975232861ad8a26340bc4b73fc
wangxy <wangxinyang1983@gmail.com>
2025-04-03 21:10:57 +0900

    fix: map styles

--- src/components/googleMap.tsx
+++ src/components/googleMap.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 
 const GoogleMap = () => {
+  const companyAddress = "神奈川県横浜市港北区新吉田東五丁目70番16号";
+  const postalCode = "223-0058";
+  const mapUrl = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=${encodeURIComponent(
+    postalCode + " " + companyAddress
+  )}&zoom=16&language=ja`;
+
   return (
-    <div className="w-full h-[500px]">
+    <div className="w-full h-[500px] relative">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.067292267553!2d139.605230!3d35.524788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60187a0fcf0f2c59%3A0xf2da94b7728f6c0d!2z5a6e5biC5rWs5rC05YyX5a2Q5Lqs5b6J5rS_!5e0!3m2!1sen!2sin!4v1626711702723"
+        src={mapUrl}
         width="100%"
         height="100%"
         style={{ border: 0 }}
         loading="lazy"
+        allowFullScreen
+        referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
     </div>
   );
